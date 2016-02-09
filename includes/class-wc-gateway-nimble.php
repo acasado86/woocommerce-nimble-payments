3b8c0d6a4a3a4da5445ea1bd08cefb948aaefc94
acasado <acasado@opensistemas.com>
2016-02-09 17:39:31 +0100

    WooCommerce Plugin for Nimble - unstable version

---
+++ includes/class-wc-gateway-nimble.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Description of WC_Gateway_Nimble
+ *
+ * @author acasado
+ */
+class WC_Gateway_Nimble extends WC_Payment_Gateway {
+    //put your code here
+    function __construct() {
+        $this->id   = 'nimble_payments_gateway';
+        $this->icon = plugins_url( 'assets/images/bbva_logo.svg', plugin_dir_path( __FILE__ ) );
+        $this->has_fields = false;
+        $this->method_title = __( 'Nimble payments', 'woocommerce-nimble-payments' );
+        $this->method_description = __( 'Description...', 'woocommerce-nimble-payments' );
+        $this->title    =   __( 'Card payment', 'woocommerce-nimble-payments' );
+        $this->supports           = array(
+			'products',
+			'refunds'
+		);
+        
+        // Load the form fields
+        $this->init_form_fields();
+
+        // Load the settings
+        $this->init_settings();
+
+        add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+        
+        
+     }
+     
+    function process_payment( $order_id ) {
+            global $woocommerce;
+            $order = new WC_Order( $order_id );
+            
+            $nimbleApi = $this->inicialize_nimble_api();
+            
+            $payment = $this->set_payment_info($order);
+            
+            $p = new Payments();
+            $response = $p->SendPaymentClient($nimbleApi, $payment);
+            
+            
+            // Mark as on-hold (we're awaiting the cheque)
+            $order->update_status('on-hold', __( 'Awaiting cheque payment', 'woocommerce' ));
+
+            // Reduce stock levels
+            $order->reduce_order_stock();
+
+            // Remove cart
+            $woocommerce->cart->empty_cart();
+
+            // Return thankyou redirect
+            return array(
+                    'result' => 'success',
+                    //'redirect' => $this->get_return_url( $order )
+                    //'redirect' => 'success/?hola=hola'
+                    'redirect' => $response["data"]["paymentUrl"]
+            );
+    }
+    
+    function inicialize_nimble_api(){
+        $params = array(
+                'clientId' => $this->get_option('seller_id'),
+                'clientSecret' => $this->get_option('secret_key'),
+                'mode' => 'demo'
+        );
+
+        /* High Level call */
+        return new NimbleAPI($params);
+    }
+    
+    function set_payment_info($order){
+        $payment = array(
+            'amount' => $order->get_total() * 100,
+            'currency' => $order->get_order_currency( ),
+            'customerData' => $order->get_order_number(),
+            'paymentSuccessUrl' => 'http://wordpress.nimble.com/success',
+            'paymentErrorUrl' => 'http://wordpress.nimble.com/error'
+        );
+        
+        return $payment;
+    }
+    
+    /**
+     * Init payment gateway form fields
+     */
+    function init_form_fields() {
+
+            $this->form_fields = array(
+                    'enabled' => array(
+                            'title'       => __( 'Enable/Disable', 'woocommerce-nimble-payments' ),
+                            'label'       => __( 'Enable Nimble Payments', 'woocommerce-nimble-payments' ),
+                            'type'        => 'checkbox',
+                            'description' => '',
+                            'default'     => 'no'
+                    ),
+                    'seller_id' => array(
+                            'title'       => __( 'Seller ID', 'woocommerce-nimble-payments' ),
+                            'type'        => 'text',
+                            'description' => __( 'Obtained from https://www.nimblepayments.com', 'woocommerce-nimble-payments' ),
+                            'default'     => '',
+                            'desc_tip'    => true
+                    ),
+                    'secret_key' => array(
+                            'title'       => __( 'Secret Key', 'woocommerce-nimble-payments' ),
+                            'type'        => 'text',
+                            'description' => __( 'Obtained from https://www.nimblepayments.com', 'woocommerce-nimble-payments' ),
+                            'default'     => '',
+                            'desc_tip'    => true
+                    ),
+                    'sandbox' => array(
+                            'title'       => __( 'Use Sandbox', 'woocommerce-nimble-payments' ),
+                            'label'       => __( 'Enable sandbox', 'woocommerce-nimble-payments' ),
+                            'type'        => 'checkbox',
+                            'description' => '',
+                            'default'     => 'no'
+                    )
+       );
+    }
+}
---
+++ woocommerce-nimble-payments.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+Plugin Name: WooCommerce Nimble Payments
+Plugin URI: https://www.nimblepayments.com
+Description: Add Nimble payment services to your WooCommmerce.
+Version: 1.0.0
+Author: acasado
+Author URI: 
+License: GPLv2
+*/
+
+/* 
+Copyright (C) 2016 acasado
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+class WoocommerceNimblePayments {
+    static $instance = null;
+
+    static function & getInstance() {
+        if (null == WoocommerceNimblePayments::$instance) {
+            WoocommerceNimblePayments::$instance = new WoocommerceNimblePayments();
+        }
+
+        return WoocommerceNimblePayments::$instance;
+    }
+    
+    function WoocommerceNimblePayments() {
+        // If instance is null, create it. Prevent creating multiple instances of this class
+        if ( is_null( self::$instance ) ) {
+            self::$instance = $this;
+            
+            add_action( 'plugins_loaded', array( $this, 'load_text_domain' ) );
+            
+            add_action( 'plugins_loaded', array( $this, 'init_your_gateway_class' ) );
+            
+            add_filter( 'woocommerce_payment_gateways', array( $this, 'add_your_gateway_class' ) );
+        }
+    }
+    
+    function activar_plugin() {
+        if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+            return;
+        }
+        wp_die('WooCommerce plugin must be installed.');
+    }
+    
+    function desactivar_plugin() {
+    }
+    
+    /**
+     * Initialise Gateway Settings Form Fields
+     */
+     function init_your_gateway_class() {
+         include_once( 'includes/class-wc-gateway-nimble.php' );
+         require_once 'lib/Nimble/base/NimbleAPI.php';
+    } // End init_form_fields()
+    
+    function add_your_gateway_class( $methods ) {
+	$methods[] = 'WC_Gateway_Nimble'; 
+	return $methods;
+    }
+    
+    function load_text_domain(){
+        load_plugin_textdomain('woocommerce-nimble-payments', null, basename(dirname(__FILE__)) . "/lang");
+    }
+}
+
+
+$oWoocommerceNimblePayments = WoocommerceNimblePayments::getInstance();
+register_activation_hook(__FILE__, array($oWoocommerceNimblePayments, 'activar_plugin'));
+register_deactivation_hook(__FILE__, array( $oWoocommerceNimblePayments, 'desactivar_plugin'));
