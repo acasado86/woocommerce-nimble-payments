0f57deb0fa3548a12156305fb7a90e39613bcee0
acasado <acasado@opensistemas.com>
2016-02-22 11:01:55 +0100

    Change plugin for using SDK version 1.0.0.1

--- woocommerce-nimble-payments.php
+++ woocommerce-nimble-payments.php
@@ -108,7 +108,7 @@ class WoocommerceNimblePayments {
      function init_your_gateway_class() {
          include_once( 'includes/class-wc-gateway-nimble.php' );
          require_once 'lib/Nimble/base/NimbleAPI.php';
-         require_once 'lib-extensions/Nimble/wordpress/WP_NimbleAPI.php';
+         require_once 'lib/Nimble/extensions/wordpress/WP_NimbleAPI.php';
     } // End init_form_fields()
     
     function add_your_gateway_class( $methods ) {
