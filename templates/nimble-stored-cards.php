7a607cdd2ed00fe35e8cbfbe5c558a635f64c937
rmartineza <rmartineza@vocento.com>
2016-05-19 18:12:59 +0200

    añadido el radio button y las imagenes de las tarjetas

--- templates/nimble-stored-cards.php
+++ templates/nimble-stored-cards.php
@@ -1,7 +1,12 @@
-<span>Usuario: <?php echo $user->ID;?></span>
-<select name="<?php echo $this->id; ?>_storedcard">
+<span>Identificador: <?php echo $user->ID;?></span><br>
     <?php foreach ($cards as $card) : ?>
-    <option <?php if ($card['default']) echo 'selected="selected"';  ?>value="<?php echo base64_encode(json_encode($card)); ?>"><?php echo $card['maskedPan'] . ', ' . $card['cardBrand']; ?></option>
+    <input  type="radio" name="<?php echo $this->id; ?>_storedcard" <?php if ($card['default']) echo "checked";  ?> value="<?php echo base64_encode(json_encode($card)); ?>">
+        <?php echo $card['maskedPan']; 
+            if( $card['cardBrand'] == 'VISA') { ?><img src="<?php echo plugins_url('assets/images/visa-logo.png', plugin_dir_path(__FILE__));?>"/>&nbsp;&nbsp;&nbsp;&nbsp;<img style="cursor: pointer;" class="delete-card" src="<?php echo plugins_url('assets/images/delete-button.png', plugin_dir_path(__FILE__));?>"/>
+            <?php } else if( $card['cardBrand'] == 'MASTERCARD') { ?><img src="<?php echo plugins_url('assets/images/mastercard-logo.png', plugin_dir_path(__FILE__));?>"/>&nbsp;&nbsp;&nbsp;&nbsp;<img  style="cursor: pointer;" class="delete-card" src="<?php echo plugins_url('assets/images/delete-button.png', plugin_dir_path(__FILE__));?>"/>
+            <?php } else { ?><img src="<?php echo plugins_url('assets/images/credit-card.png', plugin_dir_path(__FILE__));?>"/> 
+            <?php } ?>
+    </input><br>
+                
     <?php endforeach;?>
-    <option value="">Nueva tarjeta</option>
-</select>
+    <input type="radio" name="<?php echo $this->id; ?>_storedcard" value="">Nueva tarjeta <img src="<?php echo plugins_url('assets/images/credit-card.png', plugin_dir_path(__FILE__));?>"/></input>
